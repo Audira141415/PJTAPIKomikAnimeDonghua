cac34117e1bd117dc36062468a11b7820b40e2f9
Audira <learn.audira@gmail.com>
2026-04-18 06:57:24 +0700

    fix: correctly wrap aggregator results in data field to resolve null response

--- src/modules/comic/scrapers/aggregator/aggregator.controller.js
+++ src/modules/comic/scrapers/aggregator/aggregator.controller.js
@@ -5,12 +5,12 @@ const { success } = require('../../../../utils/response');
 const service = require('./aggregator.service');
 
 const sources = catchAsync(async (req, res) =>
-  success(res, service.sources()));
+  success(res, { data: service.sources() }));
 
 const latest = catchAsync(async (req, res) =>
-  success(res, await service.latest(req.query.sources, req.query.page)));
+  success(res, { data: await service.latest(req.query.sources, req.query.page) }));
 
 const search = catchAsync(async (req, res) =>
-  success(res, await service.search(req.query.q, req.query.sources, req.query.page)));
+  success(res, { data: await service.search(req.query.q, req.query.sources, req.query.page) }));
 
 module.exports = { sources, latest, search };
